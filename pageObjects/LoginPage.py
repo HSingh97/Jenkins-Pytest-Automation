d309d8d98c0c0ff47aeef9f2edadd9294c14fe6b
Harman <harmanjot.singh@gmail.com>
2022-07-11 17:05:29 +0530

    git commit

---
+++ pageObjects/LoginPage.py
@@ -0,0 +1,18 @@
+class LoginPage:
+    username_id = "luci_username"
+    password_id = "luci_password"
+    button_xpath = "/html/body/div/form/div[2]/input[1]"
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def setUserName(self, username):
+        self.driver.find_element_by_name(self.username_id).clear()
+        self.driver.find_element_by_name(self.username_id).send_keys(username)
+
+    def setPassword(self, username):
+        self.driver.find_element_by_name(self.password_id).clear()
+        self.driver.find_element_by_name(self.password_id).send_keys(username)
+
+    def clickLogin(self):
+        self.driver.find_element_by_xpath(self.button_xpath).click()
---
+++ testCases/test_login.py
@@ -0,0 +1,44 @@
+import time
+from selenium import webdriver
+from pageObjects.LoginPage import LoginPage
+
+
+class Test_001_Login:
+    URL = "http://10.0.0.1/cgi-bin/luci"
+    username = "admin"
+    password = "admin"
+
+    def test_HomePageTitle(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get(self.URL)
+        current_title = self.driver.title
+
+        if current_title == "Sify - LuCI":
+            assert True
+            self.driver.save_screenshot(".\\Screenshots\\" + current_title + ".png")
+            self.driver.close()
+
+        else:
+            self.driver.save_screenshot(".\\Screenshots\\"+"test_homePageTitle.png")
+            self.driver.close()
+            assert False
+
+    def test_Login(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get(self.URL)
+        time.sleep(2)
+        self.lp = LoginPage(self.driver)
+        self.lp.setUserName(self.username)
+        self.lp.setPassword(self.password)
+        self.lp.clickLogin()
+        current_title = self.driver.title
+
+        if current_title == "Sify - Home - LuCI":
+            assert True
+            self.driver.save_screenshot(".\\Screenshots\\" + current_title + ".png")
+            self.driver.close()
+
+        else:
+            self.driver.save_screenshot(".\\Screenshots\\" + "test_homePageTitle.png")
+            self.driver.close()
+            assert False
