37dc51c9ca044e4e23c5ee071f22bce34e496c71
Harman <harmanjot.singh@gmail.com>
2022-07-26 12:42:32 +0530

    git commit

--- pageObjects/UpgradePage.py
+++ pageObjects/UpgradePage.py
@@ -1,5 +1,6 @@
 import time
 from selenium.common.exceptions import NoSuchElementException
+from pathlib import Path
 
 
 class UpgradePage:
@@ -7,7 +8,10 @@ class UpgradePage:
     chooseImage_xpath = "//*[@id='image']"
     upgradeButton_xpath = "//*[@id='kwnupgrade']/div/div[3]/div/input[2]"
     proceedButton_xpath = "//input[@value='Proceed']"
-    firmware_path = "flasher/nor-ipq40xx-single-enc.img"
+
+    p = Path("flasher/nor-ipq40xx-single-enc.img").resolve()
+
+    firmware_path = str(p)
 
     def __init__(self, driver):
         self.driver = driver
