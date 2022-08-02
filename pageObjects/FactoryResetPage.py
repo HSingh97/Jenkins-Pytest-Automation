de5db15ee4c54e9abf2fe33f16349e3fd102b54c
Harman <harmanjot.singh@gmail.com>
2022-08-02 11:27:38 +0530

    git commit

---
+++ pageObjects/FactoryResetPage.py
@@ -0,0 +1,27 @@
+import time
+from selenium.common.exceptions import NoSuchElementException
+from pathlib import Path
+
+
+class ResetPage:
+
+    resetPage_xpath = "//*[@id='maincontent']/div/ul/li[4]/a"
+    resetProceedButton_xpath = "//*[@id='reset']/input"
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def clickResetPage(self):
+        try:
+            self.driver.find_element_by_xpath(self.resetPage_xpath).click()
+            time.sleep(1)
+        except NoSuchElementException:
+            print("No Such Element Found")
+
+    def clickProceed(self):
+        try:
+            elem = self.driver.find_element_by_xpath(self.resetProceedButton_xpath)
+            elem.click()
+            time.sleep(1)
+        except NoSuchElementException:
+            print("No Such Element Found")
---
+++ testCases/test_factoryReset.py
@@ -0,0 +1,85 @@
+import time
+import platform
+import warnings
+import subprocess
+import pytest
+
+from pageObjects.HomePage import HomePage
+from pageObjects.LoginPage import LoginPage
+from pageObjects.FactoryResetPage import ResetPage
+from utilities.readProperties import readConfig
+from testCases.configsetup import setup
+from utilities.serial_Logging import *
+
+
+URL = "http://"+readConfig.getIPaddr()+"/cgi-bin/luci"
+username = readConfig.get_username()
+password = readConfig.get_passwd()
+serial_port = readConfig.getSerialPortDevice()
+serial_port_log = readConfig.getSerialLogsDevice()
+driver = setup
+
+
+# Ignore Warnings
+def warn(*args, **kwargs):
+    pass
+
+
+warnings.warn = warn
+
+
+def test_Upgrade(driver):
+    # Start Serial Console logging for specific port
+    serial_logging_start(serial_port, serial_port_log)
+
+    driver.get(URL)
+    time.sleep(2)
+
+    lp = LoginPage(driver)
+    lp.setUserName(username)
+    lp.setPassword(password)
+    lp.clickLogin()
+    time.sleep(2)
+
+    hp = HomePage(driver)
+
+    hp.clickManagementSection()
+    hp.clickUpgradeReset()
+
+    frp = ResetPage(driver)
+    frp.clickResetPage()
+    frp.clickProceed()
+
+    time.sleep(200)
+
+    wait = 0
+    while wait < 200:
+        output = ping(readConfig.getIPaddr())
+
+        if not output:
+            wait += 3
+
+        else:
+            print("Reachable")
+            break
+
+
+    if output != 1:
+        assert False
+
+    else:
+        assert True
+
+    # Stop Serial logging
+    serial_logging_stop()
+
+    # Close the driver window
+    driver.close()
+
+
+# Ping Device with specific IP
+def ping(host):
+    param = '-n' if platform.system().lower() == 'windows' else '-c'
+    command = ['ping', param, '3', host]
+
+    return subprocess.call(command) == 0
