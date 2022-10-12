932c10ab45997d0039fb19d341fd7f5fc86e4889
Harman <harmanjot.singh@gmail.com>
2022-10-12 13:44:38 +0530

    git commit

--- testCases/configsetup.py
+++ testCases/configsetup.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import platform
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -11,7 +12,12 @@ def setup():
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    if platform.system() == "Linux" and platform.machine() == "armv7l":
+        # if raspberry pi
+        chrome_options.BinaryLocation = ("/usr/bin/chromium-browser")
+        driver = Service("/usr/bin/chromedriver")
+    else:
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     driver.set_window_position(0, 0)
     driver.set_window_size(1920, 1080)
     return driver
