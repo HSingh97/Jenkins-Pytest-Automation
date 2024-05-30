8741e5099d9783150763ea69b34f6c9263e4e128
harmanjots <harmanjots@keywestnetworks.com>
2024-05-30 10:42:36 +0530

    git commit

--- testCases/configsetup.py
+++ testCases/configsetup.py
@@ -1,9 +1,9 @@
+import pytest
 from selenium import webdriver
-import platform
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-import pytest
+import platform
 
 
 @pytest.fixture()
@@ -12,22 +12,18 @@ def setup():
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    if platform.system() == "Linux" and platform.machine() == "armv7l":
-        # if raspberry pi
-        chrome_options.BinaryLocation = ("/usr/bin/chromium-browser")
-        service = Service("/usr/bin/chromedriver")
 
+    if platform.system() == "Linux" and platform.machine() == "armv7l":
+        # Raspberry Pi specific setup
+        chrome_options.binary_location = "/usr/bin/chromium-browser"
+        service = Service("/usr/bin/chromedriver")
     else:
-        # if other OS
-        # service = Service(ChromeDriverManager().install())
-        chromedriver_path = "/tftpboot/chromedriver_linux64/chromedriver"
+        # Other OS setup
+        chromedriver_path = ChromeDriverManager().install()
         service = Service(chromedriver_path)
 
-    driver = webdriver.Chrome(
-        service=service,
-        options=chrome_options
-    )
-
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.set_window_position(0, 0)
     driver.set_window_size(1920, 1080)
-    return driver
+    yield driver
+    driver.quit()
