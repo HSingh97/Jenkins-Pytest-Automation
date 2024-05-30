29c535f17dadcf97cfa8fcce7cfc7ffec61c690e
harmanjots <harmanjots@keywestnetworks.com>
2024-05-30 14:59:48 +0530

    git commit

--- testCases/configsetup.py
+++ testCases/configsetup.py
@@ -4,6 +4,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import platform
 from webdriver_manager.chrome import ChromeDriverManager
+import tempfile
+import os
+import requests
 
 
 @pytest.fixture()
@@ -20,19 +23,24 @@ def setup():
     else:
         # Other OS setup with custom ChromeDriver URL
         custom_url = "https://storage.googleapis.com/chrome-for-testing-public/125.0.6422.76/linux64/chromedriver-linux64.zip"
-        driver_path = "/path/to/downloaded/chromedriver"
 
-        # Download the ChromeDriver using the custom URL
-        import requests
-        response = requests.get(custom_url)
-        with open(driver_path, 'wb') as file:
-            file.write(response.content)
+        # Create a temporary directory
+        with tempfile.TemporaryDirectory() as temp_dir:
+            driver_path = os.path.join(temp_dir, "chromedriver")
 
-        # Set up the service with the custom path
-        service = Service(driver_path)
+            # Download the ChromeDriver using the custom URL
+            response = requests.get(custom_url)
+            with open(driver_path, 'wb') as file:
+                file.write(response.content)
 
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-    driver.set_window_position(0, 0)
-    driver.set_window_size(1920, 1080)
-    yield driver
-    driver.quit()
+            # Ensure the downloaded file is executable
+            os.chmod(driver_path, 0o755)
+
+            # Set up the service with the custom path
+            service = Service(driver_path)
+
+            driver = webdriver.Chrome(service=service, options=chrome_options)
+            driver.set_window_position(0, 0)
+            driver.set_window_size(1920, 1080)
+            yield driver
+            driver.quit()
