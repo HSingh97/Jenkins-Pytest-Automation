126028a5e9540163767549f067554a44547b2787
harmanjots <harmanjots@keywestnetworks.com>
2024-05-31 14:46:16 +0530

    git commit

--- preMadeFunctions/pingFunction.py
+++ preMadeFunctions/pingFunction.py
@@ -1,9 +1,23 @@
 import platform
 import subprocess
 
-
 def Ping(host):
     param = '-n' if platform.system().lower() == 'windows' else '-c'
     command = ['ping', param, '3', host]
 
     return subprocess.call(command) == 0
+
+
+def check_access(host):
+    wait = 0
+    while wait < 50:
+        localping = Ping(readConfig.getIPaddr())
+
+        if not localping:
+            wait += 3
+            time.sleep(3)
+
+        else:
+            print("Able to Access, checking remote ping")
+            return 1
+    return 0
--- testCases/test_channelTest.py
+++ testCases/test_channelTest.py
@@ -1,50 +1,31 @@
 import time
 import warnings
 import pytest
-from pageObjects.HomePage import HomePage
-from pageObjects.LinkStatsPage import StatsPage
 from utilities.readProperties import readConfig
 from testCases.configsetup import setup
 from utilities.serial_Logging import *
 from preMadeFunctions import pingFunction
-from preMadeFunctions import accessWeb
-
+import os
 
 def test_channelconnectivity():
 
     time.sleep(2)
     print("Checking Local Ping")
 
-    wait = 0
-    while wait < 50:
-        localping = pingFunction.Ping(readConfig.getIPaddr())
-
-        if not localping:
-            wait += 3
-            time.sleep(3)
-
+    if pingFunction.check_access(readConfig.getIPaddr()):
+        print("Able to Access, checking remote ping")
+        if pingFunction.check_access(readConfig.getRemoteIPaddr()):
+            print("Able to Access Remote Device ")
         else:
-            print("Able to Access, checking remote ping")
-            while wait < 50:
-                remoteping = pingFunction.Ping(readConfig.getRemoteIPaddr())
+            print("Unable to access Remote Device")
+    else:
+        print("Unable to access Local Device")
 
-                if not remoteping:
-                    wait += 3
-                    time.sleep(3)
-
-                else:
-                    print("Able to Access Remote Device ")
-                    break
-            break
-
-    time.sleep(2)
-
-    if remoteping != 1:
+    if pingFunction.check_access(readConfig.getRemoteIPaddr()) != 1:
         assert False
 
     else:
         assert True
-
 
 
 def warn(*args, **kwargs):
