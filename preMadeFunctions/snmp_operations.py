463eb3a533b7017f67e730581816c3dce971147c
HSingh <harmanjot.singh@senao.com>
2025-05-08 16:38:42 +0530

    Channel Script - Bug Fix

--- preMadeFunctions/snmp_operations.py
+++ preMadeFunctions/snmp_operations.py
@@ -10,7 +10,7 @@ def change_bandwidth(host, radio_ind, bandwidth):
 
     # Apply the configuration
     os.system("snmpset -v 2c -c private {} .1.3.6.1.4.1.52619.1.2.1.1.0 i 1".format(host))
-    time.sleep(10)
+    time.sleep(60)
 
 
 def get_snmp_value(host, oid):
