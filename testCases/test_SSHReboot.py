a69230d9a086b02f91ef30651c710c9ef537f805
Puneet Sharma <puneetkumarsharma1610@gmail.com>
2025-09-24 09:31:30 +0530

    Soft Reboot - SSH

---
+++ testCases/test_SSHReboot.py
@@ -0,0 +1,43 @@
+import time
+import warnings
+import pytest
+from testCases.conftest import local_ip
+from preMadeFunctions import pingFunction, execute_ssh_command
+
+USERNAME = "root"
+PASSWORD = "admin"
+
+
+@pytest.mark.reboot
+def test_reboot(local_ip, remote_ip):
+
+    print(f"Local IP Address: {local_ip}")
+    print(f"Remote IP Address: {remote_ip}")
+
+
+    execute_ssh_command.perform_operation(local_ip, USERNAME, PASSWORD, "reboot")
+
+    print("Waiting for device to reboot...")
+    time.sleep(60)
+
+    wait = 0
+    output = None
+    while wait < 50:
+        output = pingFunction.Ping(local_ip)
+
+        if not output:
+            wait += 3
+            time.sleep(3)
+        else:
+            print("Device is reachable again ✅")
+            break
+
+    assert output == 1, "Device did not come back online after reboot"
+
+
+
+def warn(*args, **kwargs):
+    pass
+
+
+warnings.warn = warn
