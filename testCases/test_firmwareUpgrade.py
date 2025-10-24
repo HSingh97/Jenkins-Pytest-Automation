95a8a69f403963205c10ff6b79bd6edfe7ad872b
HSingh <harmanjot.singh@senao.com>
2025-10-24 18:35:51 +0530

    script update - FW Upgrade Test

--- testCases/test_firmwareUpgrade.py
+++ testCases/test_firmwareUpgrade.py
@@ -98,7 +98,7 @@ def test_Upgrade(driver, local_ip, remote_ip,serialPort, iter):
             print("!!!! FW Upload Successful !!!!")
 
         up.clickProceed()
-        time.sleep(180)
+        time.sleep(250)
         # ssh_operations.ssh_get(local_ip, "cfg80211tool ath1 g_kwnpkt")
 
         perform_ping_check(local_ip, remote_ip, test_iteration_result)
